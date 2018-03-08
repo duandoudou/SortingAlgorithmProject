3354f8e7dd784aa54920a8f93df0eedbaa178d00
chaixinli <chaixinli0813@163.com>
2018-03-08 21:29:56 +0800

    完成冒泡排序

--- src/com/doudou/sorting/BubbleSorting.java
+++ src/com/doudou/sorting/BubbleSorting.java
@@ -6,18 +6,35 @@ package com.doudou.sorting;
  * User: Administrator
  * Date: 2018/3/8
  * Time: 16:59
+ *
  * @author duandoudou
  */
 public class BubbleSorting {
 
     /**
      * 冒泡排序实现
+     *
      * @param numbers 排序数组
      */
-    public static void sort(int[] numbers){
-        if(numbers == null || numbers.length == 0){
+    public static void sort(int[] numbers) {
+        //判断是否是空数组
+        if (numbers == null || numbers.length == 0) {
             return;
         }
-        if()
+        //判断是否是单元素数组
+        if (numbers.length == 1) {
+            return;
+        }
+        //获取长度
+        int length = numbers.length;
+        for (int i = 0; i < length - 1; i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
     }
 }
--- src/com/doudou/test/Test.java
+++ src/com/doudou/test/Test.java
@@ -8,12 +8,17 @@ import com.doudou.sorting.*;
  * User: Administrator
  * Date: 2018/3/8
  * Time: 17:26
+ *
  * @author duandoudou
  */
 public class Test {
-    public static void main(String... args){
+    public static void main(String... args) {
         int[] sortTempNums = new int[]{36, 44, 2, 17, 24, 30, 31, 44, 21, 22, 31, 37, 45, 2, 37, 11, 13, 12, 47};
 
         BubbleSorting.sort(sortTempNums);
+
+        for (int num : sortTempNums) {
+            System.out.print(num + ", ");
+        }
     }
 }
