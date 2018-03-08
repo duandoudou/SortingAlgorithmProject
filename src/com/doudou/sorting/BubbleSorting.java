c1540244acf3665c335adb4485f2438d0b7b7e9d
段豆豆 <duandoudou2011@gmail.com>
2018-03-08 17:31:58 +0800

    修改

--- src/com/doudou/sorting/BubbleSorting.java
+++ src/com/doudou/sorting/BubbleSorting.java
@@ -9,16 +9,15 @@ package com.doudou.sorting;
  * @author duandoudou
  */
 public class BubbleSorting {
-    public static void main(String... args){
-        int[] sortTempNums = new int[]{36, 44, 2, 17, 24, 30, 31, 44, 21, 22, 31, 37, 45, 2, 37, 11, 13, 12, 47};
-
-    }
 
     /**
      * 冒泡排序实现
      * @param numbers 排序数组
      */
     public static void sort(int[] numbers){
-
+        if(numbers == null || numbers.length == 0){
+            return;
+        }
+        if()
     }
 }
---
+++ src/com/doudou/test/Test.java
@@ -0,0 +1,19 @@
+package com.doudou.test;
+
+import com.doudou.sorting.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * VisuAlgoProject
+ * User: Administrator
+ * Date: 2018/3/8
+ * Time: 17:26
+ * @author duandoudou
+ */
+public class Test {
+    public static void main(String... args){
+        int[] sortTempNums = new int[]{36, 44, 2, 17, 24, 30, 31, 44, 21, 22, 31, 37, 45, 2, 37, 11, 13, 12, 47};
+
+        BubbleSorting.sort(sortTempNums);
+    }
+}
