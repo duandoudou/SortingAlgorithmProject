347725365cfc0577b7c707329ca32c1f6f5c68f9
段豆豆 <duandoudou2011@gmail.com>
2018-03-09 09:18:20 +0800

    完成选择排序算法

---
+++ src/com/doudou/sorting/SelectionSorting.java
@@ -0,0 +1,44 @@
+package com.doudou.sorting;
+
+import com.doudou.util.SortingPrintUtil;
+
+/**
+ * Created by IntelliJ IDEA.
+ * VisuAlgoProject
+ * User: Administrator
+ * Date: 2018/3/9
+ * Time: 8:28
+ *
+ * @author duandoudou
+ */
+public class SelectionSorting {
+
+    /**
+     * 选择排序算法实现
+     *
+     * @param numbers 排序数组
+     */
+    public static void sort(int[] numbers) {
+        int length = numbers.length;
+        for (int i = 0; i < length - 1; i++) {
+            //选取最小值
+            int min = numbers[i];
+            //设置初始索引
+            int index = i;
+            for (int j = i + 1; j < length; j++) {
+                if (numbers[j] < min) {
+                    min = numbers[j];
+                    index = j;
+                }
+            }
+            //交换
+            if (index != i) {
+                int temp = numbers[index];
+                numbers[index] = numbers[i];
+                numbers[i] = temp;
+            }
+
+            SortingPrintUtil.print(numbers, i + 1);
+        }
+    }
+}
